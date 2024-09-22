b1850b5f4b51e105fc33b59f5253bd691ea60855
Arina Zubova <arina777pototskaya@gmail.com>
2024-09-22 16:02:56 +0400

    test

--- src/main/java/driver/impl/DriverFactoryImpl.java
+++ src/main/java/driver/impl/DriverFactoryImpl.java
@@ -12,11 +12,9 @@ public class DriverFactoryImpl implements DriverFactory {
     @Override
     public WebDriver createDriverInstance() throws DriverNotSupportedException {
         if (browserType.equals("chrome")) {
-            System.err.println("CHROME xChromeWebDriver");
             return ChromeWebDriver.createDriver();
         }
         if (browserType.equals("ci")) {
-            System.err.println("CI xChromeWebDriver");
             return ChromeWebDriver.createCiDriver();
         }
         throw new DriverNotSupportedException(browserType);
